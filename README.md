b9bd779b83b89344bf36579a1338982c5d8ac762
dokhosalvi <camarasalvi@gmail.com>
2021-09-26 18:16:45 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-rdxxq8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-rdxxq8)
---
+++ src/components/FeaturedMovie.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './FeaturedMovie.css';
+
+export default ({item}) => {
+  return (
+    <section className="featured" style={{
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundImage: 'url(https://image.tmdb.org/t/p/original${item.backdrop_path})'
+    }}>
+      <div className="featured--vertical">
+        
+      </div>
+      <div>{item.original_name}</div> 
+    </section>
+  );
+}
